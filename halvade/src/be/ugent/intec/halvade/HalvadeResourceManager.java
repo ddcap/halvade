4462fe5e243baf8a947c2717378dde6b76c2d0a5
Dries Decap <driesdecap@gmail.com>
2016-01-23 16:11:33 +0100

    reset the memory for the map tasks

--- halvade/src/be/ugent/intec/halvade/HalvadeResourceManager.java
+++ halvade/src/be/ugent/intec/halvade/HalvadeResourceManager.java
@@ -97,7 +97,7 @@ public class HalvadeResourceManager {
         else 
             conf.set("mapreduce.reduce.cpu.vcores", "" + opt.rthreads );
         conf.set("mapreduce.reduce.memory.mb", "" + rmem);        
-        conf.set("mapreduce.reduce.java.opts", "-Xmx" + (int)(0.8*rmem) + "m");
+        conf.set("mapreduce.reduce.java.opts", "-Xmx" + (int)(0.18*rmem) + "m");
         conf.set("mapreduce.map.java.opts", "-Xmx" + (int)(0.18*mmem) + "m");
         conf.set("mapreduce.job.reduce.slowstart.completedmaps", "0.99");
         
